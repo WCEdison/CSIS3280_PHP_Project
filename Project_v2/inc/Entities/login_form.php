4d3b40396b980f608d456107b6b81c28d5b1af44
Manj-Dilraj <122954040+Manj-Dilraj@users.noreply.github.com>
2023-07-31 22:43:30 -0700

    final commit

--- Project_v2/inc/Entities/login_form.php
+++ Project_v2/inc/Entities/login_form.php
@@ -18,10 +18,10 @@
 	<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
 
 	<!-- Custom stylesheets -->
-	<link type="text/css" rel="stylesheet" href="css/style.css" />
-	<link type="text/css" rel="stylesheet" href="css/accountbtn.css" />
-	<link type="text/css" rel="stylesheet" href="css/more-styles.css" />
-	<link type="text/css" rel="stylesheet" href="css/searchInfo.css" />
+	<link type="text/css" rel="stylesheet" href="../../css/style.css" />
+	<link type="text/css" rel="stylesheet" href="../../css/accountbtn.css" />
+	<link type="text/css" rel="stylesheet" href="../../css/more-styles.css" />
+	<link type="text/css" rel="stylesheet" href="../../css/searchInfo.css" />
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
